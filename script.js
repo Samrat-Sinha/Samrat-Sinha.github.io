7651bc049e6f6fedacd19a60974e941e0f4c6040
samrat <smrtsnh1995@gmail.com>
2022-10-26 16:41:30 +0530

    Assignment Day-1 Week-3

--- script.js
+++ script.js
@@ -37,37 +37,7 @@ $(document).ready(function(){
     var typed = new Typed(".typing", {
         strings: ["Java Developer"],
         typeSpeed: 100,
-        backSpeed: 60,
+        backSpeed: 80,
         loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Java Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
     });
 });
